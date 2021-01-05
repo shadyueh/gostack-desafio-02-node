d36bf7fe3cb90b825c670accaf835c390f3171a6
Held Grijo <held.grijo@gmail.com>
2021-01-04 23:20:48 -0400

    Likes

--- src/app.js
+++ src/app.js
@@ -60,7 +60,17 @@ app.delete("/repositories/:id", (request, response) => {
 
 // LIKE
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({"error":"Repository not found."})
+  }
+
+  repositories[repoIndex].likes += 1;
+
+  return response.json(repositories[repoIndex]); 
+
 });
 
 module.exports = app;
